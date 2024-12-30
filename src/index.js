2ce8f090ec1931701ecfc69d51d9d5142cb45b86
Bobby Aston <bobby.j.aston@gmail.com>
2024-12-30 18:41:13 +0900

    fixed missing template.html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import css from "./style/style.css";
+import "./style/style.css";
 import {initializeMenuButtons} from "./modules/menuButtons";
 
 initializeMenuButtons();
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+  mode: "development",
   devtool: "eval-source-map",
   devServer: {
     static: "./dist",
+    watchFiles: ["./src/template.html"],
   },
-  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ]
 };
